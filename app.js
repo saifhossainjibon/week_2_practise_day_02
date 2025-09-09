1476e4bb39b27793fa2001d3a5c92fca9187cd3a
Saif Hossain Jibon <saifhossainjibon@gmail.com>
2025-09-09 19:51:21 +0600

    completed

--- app.js
+++ app.js
@@ -6,30 +6,38 @@ const showSearchMeal = () => {
   const input = document.getElementById("input-field");
   const inputFieldText = input.value;
   input.value = "";
-//   console.log(inputFieldText);
-  fetch(
-    `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputFieldText}`
-  )
-    .then((res) => res.json())
-    .then((data) => displayBooks(data.meals));
+  showDetails.innerHTML = "";
+  if (inputFieldText === "") {
+    document.getElementById("error-text").innerText = "Please write a meal name";
+    bookContainer.innerHTML = "";
+    resultDiv.textContent = "";
+  } else {
+    fetch(
+      `https://www.themealdb.com/api/json/v1/1/search.php?s=${inputFieldText}`
+    )
+      .then((res) => res.json())
+      .then((data) => displayBooks(data.meals));
+  }
 };
 
 const displayBooks = (meals) => {
   if (!meals) {
-    errorText.innerText =
-      'No meals found';
+    errorText.innerText = "No meals found";
     return;
   }
-  errorText.innerText=""
+  errorText.innerText = "";
   mealContainer.textContent = "";
   meals.forEach((meal) => {
     const div = document.createElement("div");
     div.classList.add("col");
     div.innerHTML = `
-      <div class="card pb-4" style="width: 15rem;" onclick="showMealDetails('${meal.strMealThumb}',
-      '${meal.strMeal}'
+      <div class="card pb-4" style="width: 15rem;" onclick="showMealDetails('${
+        meal.idMeal
+      }',
       )">
-        <img src="${meal.strMealThumb}" class="card-img-top" alt="${meal.strMeal}" />
+        <img src="${meal.strMealThumb}" class="card-img-top" alt="${
+      meal.strMeal
+    }" />
         <div class="card-body">
           <h5 class="card-title">${meal.strMeal}</h5>
           <p class="card-text mb-1"> ${
@@ -42,17 +50,33 @@ const displayBooks = (meals) => {
   });
 };
 
-const showMealDetails=(pic, title)=>{
-    // console.log(meal)
-    const div = document.createElement("div");
-    div.innerHTML = `
-      <div class="mx-auto card pb-4 m-4" style="width: 25rem;">
-        <img src="${pic}" class="card-img-top" />
+const showMealDetails = (mealId) => {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+      let ingredientsList = [];
+      for (let i = 1; i <= 20; i++) {
+        const ingredient = meal[`strIngredient${i}`];
+        if (ingredient) {
+          ingredientsList.push(ingredient);
+        }
+      }
+      const div = document.createElement("div");
+      div.innerHTML = `
+        <div class="mx-auto card pb-4 m-4" style="width: 22rem;">
         <div class="card-body">
-          <h5 class="card-title">${title}</h5>
+        <img src="${meal.strMealThumb}" class="card-img-top"/>
+            <h5 class="card-title pt-2">${meal.strMeal}</h5>
+            <p class="card-text mb-1">Ingredients:</p>
+            <ul>
+              ${ingredientsList.map((item) => `<li>${item}</li>`).join("")}
+            </ul>
+          </div>
         </div>
-      </div>
-    `;
-    showDetails.innerHTML = "";
-    showDetails.appendChild(div);
-}
+      `;
+
+      showDetails.innerHTML = "";
+      showDetails.appendChild(div);
+    });
+};
